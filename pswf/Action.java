824ee506e9585e0969e977045542c0430b0a9186
zhangqun <728292987@qq.com>
2018-11-03 13:47:10 +0800

    no message

---
+++ pswf/Action.java
@@ -0,0 +1,75 @@
+import java.io.Serializable;
+
+public class Action implements Serializable {
+
+    private static final long serialVersionUID = 2020972190295872459L;
+
+    private Long id;
+    private String actionCode;
+    private String actionName;
+    private String actionUrl;
+    private String actionType;
+    private Long moduleId;
+    private String descr;
+
+    public Action() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getActionCode() {
+        return actionCode;
+    }
+
+    public void setActionCode(String actionCode) {
+        this.actionCode = actionCode;
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public void setActionName(String actionName) {
+        this.actionName = actionName;
+    }
+
+    public String getActionUrl() {
+        return actionUrl;
+    }
+
+    public void setActionUrl(String actionUrl) {
+        this.actionUrl = actionUrl;
+    }
+
+    public String getActionType() {
+        return actionType;
+    }
+
+    public void setActionType(String actionType) {
+        this.actionType = actionType;
+    }
+
+    public Long getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(Long moduleId) {
+        this.moduleId = moduleId;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+}
