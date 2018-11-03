86449acea5c4cb5a2195d360c2b12400448428d4
zhangqun <728292987@qq.com>
2018-11-03 13:53:52 +0800

    李四提交Module

---
+++ pswf/Module.java
@@ -0,0 +1,78 @@
+import java.io.Serializable;
+import java.util.List;
+
+
+// this is lisi !
+public class Module implements Serializable {
+
+    private static final long serialVersionUID = 2901703856123441317L;
+
+    private Long id;
+    private String moduleCode;
+    private String moduleName;
+    private String moduleUrl;
+    private String moduleType;
+    private Long navigationId;
+    private String descr;
+
+    public Module() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModuleCode() {
+        return moduleCode;
+    }
+
+    public void setModuleCode(String moduleCode) {
+        this.moduleCode = moduleCode;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getModuleUrl() {
+        return moduleUrl;
+    }
+
+    public void setModuleUrl(String moduleUrl) {
+        this.moduleUrl = moduleUrl;
+    }
+
+    public String getModuleType() {
+        return moduleType;
+    }
+
+    public void setModuleType(String moduleType) {
+        this.moduleType = moduleType;
+    }
+
+    public Long getNavigationId() {
+        return navigationId;
+    }
+
+    public void setNavigationId(Long navigationId) {
+        this.navigationId = navigationId;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+}
